dc3c8cda6547546e043e278acd69ff1e28d3d375
eindrius <andrews.ana@aluno.unip.br>
2022-03-27 20:18:23 -0300

    mudança pequena na responsividade

--- js/owl/main.js
+++ js/owl/main.js
@@ -1,22 +1,23 @@
-/* $('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 3
-        },
-        1000: {
-            items: 5
-        }
-    }
-}) */
 $('.owl-carousel').owlCarousel({
-    margin: 10,
-    loop: true,
-    autoWidth: true,
-    items: 8
-})
+        loop: true,
+        margin: 10,
+        nav: false,
+        autoWidth: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
+    /*$('.owl-carousel').owlCarousel({
+        margin: 10,
+        loop: true,
+        autoWidth: true,
+        items: 8
+    })*/
