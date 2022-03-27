1f1241106faa618df3ecc96b76253420b2f7634a
eindrius <andrews.ana@aluno.unip.br>
2022-03-27 18:51:17 -0300

    Commit Inicial

---
+++ js/owl/main.js
@@ -0,0 +1,22 @@
+/* $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 3
+        },
+        1000: {
+            items: 5
+        }
+    }
+}) */
+$('.owl-carousel').owlCarousel({
+    margin: 10,
+    loop: true,
+    autoWidth: true,
+    items: 8
+})
